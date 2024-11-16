3de785ed124d71eac2ed42891689d19213ddb685
Daniele Senko <daniele.senko@unemat.br>
2024-11-16 01:45:09 -0400

    refactor: port modifies

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.post("/usuarios", async (req, res) => {
   await prisma.user.create({
